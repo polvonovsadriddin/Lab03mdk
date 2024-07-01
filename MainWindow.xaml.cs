a4b359a8e598af2803b93d1dfeac47b996cbcc44
polvonovsadriddin <144240322+polvonovsadriddin@users.noreply.github.com>
2024-07-02 01:32:59 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace lab3mdk
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Result.Text = "";
+
+            Cursor cursor = new Cursor();
+            cursor = cursor + (int.Parse(korX.Text), int.Parse(korY.Text));
+            Result.Text += ($"Координаты курсора: X = {cursor.X}, Y = {cursor.Y}");
+            korX.Text = "";
+            korY.Text = "";
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Result.Text = "";
+
+            Cursor cursor = new Cursor();
+
+            if (int.TryParse(SizeKurs.Text, out int number))
+            {
+                if (number <= 15)
+                {
+                    cursor.Size = int.Parse(SizeKurs.Text);
+                    Result.Text += ($"размер курсора:{cursor.Size}");
+                    SizeKurs.Text = "";
+                }
+                else
+                {
+                    SizeKurs.Text = "";
+
+                    MessageBox.Show("Веденные данные должы быть не больше 15");
+                }
+            }
+            else
+            {
+                SizeKurs.Text = "";
+                MessageBox.Show("Веденные данные должны быть числами");
+
+            }
+
+
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Result.Text = "";
+            Cursor cursor = new Cursor();
+            
+           if (Visibility.Text == "виден")
+            {
+                cursor.ShowCursor();
+                Result.Text += ($"Видиомсть курсора:{cursor.Visible}");
+            }
+           else if (Visibility.Text == "не виден")
+            {cursor.HideCursor();
+                Result.Text += ($"Видиомсть курсора:{cursor.Visible}");
+            }
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Result.Text = "";
+            Cursor cursor = new Cursor();
+            if (ViewKurs.Text != "горизонтальный" && ViewKurs.Text != "вертикальный")
+            {
+                MessageBox.Show("Вы можете выбирать одну из видов курсора: горизонтальный или вертикальный");
+               ViewKurs.Text = "";
+            }
+            else {
+                cursor.Orientation = ViewKurs.Text;
+                Result.Text += ($"Вид курсора: {cursor.Orientation}");
+                ViewKurs.Text = "";
+            }
+            ViewKurs.Text = "";
+        }
+    }
+}
---
+++ Position.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace lab3mdk
+{
+
+
+    public class Cursor
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Orientation { get; set; }
+        public int Size { get; set; }
+        public string Visible { get; set; }
+
+        public Cursor(int x = 0, int y = 0, string orientation = "horizontal", int size = 1)
+        {
+            X = x;
+            Y = y;
+            Orientation = orientation;
+            Size = size;
+            Visible = "Отсутствует";
+        }
+
+        public static Cursor operator +(Cursor cursor, (int x, int y) coordinates)
+        {
+            cursor.X += coordinates.x;
+            cursor.Y += coordinates.y;
+            return cursor;
+        }
+
+        public static Cursor operator -(Cursor cursor, (int x, int y) coordinates)
+        {
+            cursor.X -= coordinates.x;
+            cursor.Y -= coordinates.y;
+            return cursor;
+        }
+
+        public static implicit operator Cursor(string orientation)
+        {
+            return new Cursor() { Orientation = orientation };
+        }
+
+        public static implicit operator Cursor(int size)
+        {
+            return new Cursor() { Size = size };
+        }
+
+        public void HideCursor()
+        {
+            Visible = "Не виден";
+        }
+
+        public void ShowCursor()
+        {
+            Visible =  "Виден";
+        }
+    }
+}
